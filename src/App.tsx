f82f94a5460beb2002de39584adb84c1fbe1f550
Felipe de Oliveira Souza <felipe.oliveirac8@gmail.com>
2024-04-20 19:00:13 -0300

    feat: add react query

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,18 @@
+import { QueryClientProvider } from '@tanstack/react-query';
 import { MantineProvider } from '@mantine/core';
 import { Router } from './Router';
 import { theme } from './theme';
+import queryClient from './config/react-query';
 
 import '@mantine/core/styles.css';
 import './global.css';
 
 export default function App() {
   return (
-    <MantineProvider theme={theme}>
-      <Router />
-    </MantineProvider>
+    <QueryClientProvider client={queryClient}>
+      <MantineProvider theme={theme}>
+        <Router />
+      </MantineProvider>
+    </QueryClientProvider>
   );
 }

2a1d9a5a3adb78aebbec0508090c992e33e5106a
Felipe de Oliveira Souza <felipe.oliveirac8@gmail.com>
2024-04-20 18:39:12 -0300

    feat: type environment variables

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,1 +1,12 @@
 /// <reference types="vite/client" />
+
+interface ImportMetaEnv {
+  readonly VITE_API_URL: string;
+  readonly VITE_APP_TITLE: string;
+  readonly VITE_APP_DESCRIPTION: string;
+  readonly VITE_JWT_TOKEN_KEY: string;
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv;
+}
